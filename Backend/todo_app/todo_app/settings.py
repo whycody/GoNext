78cc54fe699b1debd0369a0d6c5c8221d8a93e90
Paweł Hebda <150199572+pawheb@users.noreply.github.com>
2025-04-14 13:05:31 +0000

    Change settings.py

--- Backend/todo_app/todo_app/settings.py
+++ Backend/todo_app/todo_app/settings.py
@@ -25,13 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'cMojtg2nC5RHqTa463tJr6TMhsnGjY8x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gonext-a7hthre4g0avd7fr.polandcentral-01.azurewebsites.net']
 CSRF_TRUSTED_ORIGINS = ['https://gonext-a7hthre4g0avd7fr.polandcentral-01.azurewebsites.net']
+
+# gonext-a7hthre4g0avd7fr.polandcentral-01.azurewebsites.net
+# gonext-a7hthre4g0avd7fr.polandcentral-01.azurewebsites.net
 
 AUTH_USER_MODEL = 'todos.CustomUser' 
 
