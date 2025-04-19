f8e5162d8375e843b5bb5e8d20aba8c3aef84ea9
izabelakolodziejska <izabela.kol@protonmail.com>
2025-04-19 17:55:42 +0200

    more info in group view

--- Frontend/screens/GroupsScreen.tsx
+++ Frontend/screens/GroupsScreen.tsx
@@ -1,36 +1,33 @@
-import { FlatList, StyleSheet, View } from "react-native";
-import GroupsHeader from "../components/GroupsHeader"; 
-import GroupsView from "../components/GroupsView"; 
-import { useGroups } from "../hooks/useGroups"; 
+import React from "react";
+import { FlatList, StyleSheet, View, Text } from "react-native";
+import GroupsHeader from "../components/GroupsHeader";
+import { useGroupStats } from "../hooks/useGroupStats";
 import { useTheme } from "@react-navigation/native";
 
 const GroupsScreen = () => {
-  const groups = useGroups(); 
+  const groupStats = useGroupStats(); // Pobierz statystyki grup
   const { colors } = useTheme();
   const styles = getStyles(colors);
 
-  const handleGroupPress = (id: number) => {
-    console.log("Group pressed:", id);
-  };
-
-  const handleGroupLongPress = (id: number) => {
-    console.log("Group long pressed:", id);
-  };
-
   return (
     <View style={styles.container}>
-      <GroupsHeader style={{ paddingHorizontal: 20, paddingTop: 30, paddingBottom: 30 }} /> 
+      <GroupsHeader style={{ paddingHorizontal: 20, paddingTop: 30, paddingBottom: 30 }} />
+      <View style={styles.separator} />
       <FlatList
-        style={{ marginTop: 2 }}
-        data={groups}
+        data={groupStats}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item, index }) => (
-          <GroupsView
-            index={index}
-            group={item}
-            onGroupPress={handleGroupPress}
-            onLongPress={handleGroupLongPress}
-          />
+        renderItem={({ item }) => (
+          <View style={styles.groupContainer}>
+            <View style={[styles.iconContainer, { backgroundColor: item.color }]}>
+              <Text style={styles.icon}>{item.icon}</Text>
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.groupName}>{item.name}</Text>
+              <Text style={styles.groupStats}>
+                {item.taskCount} tasks • {item.memberCount} members
+              </Text>
+            </View>
+          </View>
         )}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
       />
@@ -41,12 +38,42 @@ const GroupsScreen = () => {
 const getStyles = (colors: any) =>
   StyleSheet.create({
     container: {
-      flex: 1,
       backgroundColor: colors.background,
+      
+    },
+    groupContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      padding: 15,
+      backgroundColor: colors.card,
+    },
+    iconContainer: {
+      height: 40,
+      width: 40,
+      borderRadius: 20,
+      justifyContent: "center",
+      alignItems: "center",
+      marginRight: 15,
+    },
+    icon: {
+      fontSize: 20,
+    },
+    textContainer: {
+      flex: 1,
+    },
+    groupName: {
+      fontSize: 16,
+      fontWeight: "bold",
+      color: colors.text,
+    },
+    groupStats: {
+      fontSize: 14,
+      color: colors.text,
+      opacity: 0.6,
     },
     separator: {
-      height: 1, 
-      backgroundColor: colors.background,
+      height: 1,
+      backgroundColor: colors.border,
     },
   });
 
